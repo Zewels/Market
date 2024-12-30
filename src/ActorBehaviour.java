f1bed9feb712a1674c532d3a6a8c607fbaa43d80
Zewels <tzaznoba@gmail.com>
2024-12-30 11:16:11 +0300

    Материал с лекции

---
+++ src/ActorBehaviour.java
@@ -0,0 +1,8 @@
+public interface ActorBehaviour
+{
+    void setMakeOrder(boolean makeOrder);
+    void setTakeOrder(boolean takeOrder);
+    boolean isMakeOrder();
+    boolean isTakeOrder();
+
+}
---
+++ src/Market.java
@@ -0,0 +1,41 @@
+import java.util.List;
+
+public class Market implements MarketBehaviour,QueueBehaviour{
+
+    private List<Actor> queue;
+
+    @Override
+    public void acceptToMarket(Actor actor) {
+
+    }
+
+    @Override
+    public void releaseFromMarket(List<Actor> actorList) {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void takeInQueue(Actor actor) {
+
+    }
+
+    @Override
+    public void makeOrders() {
+
+    }
+
+    @Override
+    public void giveOrders() {
+
+    }
+
+    @Override
+    public void releaseFromQueue() {
+
+    }
+}
